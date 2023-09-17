30a5d4773e83bc7ba893a2e0bee42ca22b3fd0a4
v.boursinos <v.boursinos@turintech.ai>
2023-09-17 12:00:30 +0100

    chore: add pom plugin in order to install dependencies in the fat jar

--- src/main/java/PythonInteraction.java
+++ src/main/java/PythonInteraction.java
@@ -10,22 +10,22 @@ public class PythonInteraction {
         String script = "/home/vasilis/PycharmProjects/model-catalog-py/src/main.py";
         ProcessBuilder pb;
         Process p;
-//        pb =
-//                new ProcessBuilder(
-//                        "bash",
-//                        "-c",
-//                        String.format(
-//                                "python3 %s",
-//                                script))
-//                        .redirectErrorStream(true)
-//                        .inheritIO();
-//        pb.directory(Paths.get(".").toFile());
-//        System.out.println("Conversion script is running");
-//        p = pb.start();
-//        if (p.waitFor() != 0) {
-//            System.err.println("An error ocurred executing the conversion script.");
-//            throw new IOException(p.getErrorStream().toString());
-//        }
+        pb =
+                new ProcessBuilder(
+                        "bash",
+                        "-c",
+                        String.format(
+                                "python3 %s",
+                                script))
+                        .redirectErrorStream(true)
+                        .inheritIO();
+        pb.directory(Paths.get(".").toFile());
+        System.out.println("Conversion script is running");
+        p = pb.start();
+        if (p.waitFor() != 0) {
+            System.err.println("An error ocurred executing the conversion script.");
+            throw new IOException(p.getErrorStream().toString());
+        }
         System.out.println("Script executed successfully");
         CreateSqlScript createSqlScript = new CreateSqlScript();
         createSqlScript.createFiles();
