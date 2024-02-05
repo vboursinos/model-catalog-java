453f4d37f535bcd51afb720fdef4fac17cb97eba
v.boursinos <v.boursinos@turintech.ai>
2024-02-05 17:05:21 +0000

    chore: update queries with rev_info

--- src/main/java/scripts/inserts/model/InsertModelTable.java
+++ src/main/java/scripts/inserts/model/InsertModelTable.java
@@ -1,8 +1,9 @@
 package scripts.inserts.model;
 
-import java.util.List;
 import model.EnsembleFamily;
 import model.Model;
+
+import java.util.List;
 
 public class InsertModelTable {
 
@@ -21,20 +22,35 @@ public class InsertModelTable {
       }
     }
 
-    return String.format(
-        "INSERT INTO model(name, ml_task_id, description, display_name, structure_id, advantages, disadvantages, enabled, ensemble_type_id, family_type_id, decision_tree, model_type_id) VALUES ('%s', (select id from ml_task_type where name='%s'),'%s', '%s', (select id from model_structure_type where name='%s'), '%s', '%s', %b, (select id from model_ensemble_type where name='%s'),(select id from model_family_type where name='%s'), %b, (select id from model_type where name='%s'));\n",
-        model.getName(),
-        model.getMlTask(),
-        model.getMetadata().getModelDescription(),
-        model.getMetadata().getDisplayName(),
-        model.getMetadata().getStructure(),
-        advantagesArray,
-        disadvantagesArray,
-        !model.isBlackListed(),
-        ensembleType,
-        familyType,
-        model.getMetadata().getSupports().getDecisionTree(),
-        model.getMetadata().getModelType().get(0));
+    String modelInsertQuery =
+        String.format(
+            "INSERT INTO model(name, ml_task_id, description, display_name, structure_id, advantages, disadvantages, enabled, ensemble_type_id, family_type_id, decision_tree, model_type_id) VALUES ('%s', (select id from ml_task_type where name='%s'),'%s', '%s', (select id from model_structure_type where name='%s'), '%s', '%s', %b, (select id from model_ensemble_type where name='%s'),(select id from model_family_type where name='%s'), %b, (select id from model_type where name='%s'));\n",
+            model.getName(),
+            model.getMlTask(),
+            model.getMetadata().getModelDescription(),
+            model.getMetadata().getDisplayName(),
+            model.getMetadata().getStructure(),
+            advantagesArray,
+            disadvantagesArray,
+            !model.isBlackListed(),
+            ensembleType,
+            familyType,
+            model.getMetadata().getSupports().getDecisionTree(),
+            model.getMetadata().getModelType().get(0));
+
+//    String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
+//    int rev = InsertStaticTables.getRev();
+//    modelInsertQuery =
+//        modelInsertQuery.concat(
+//            "INSERT INTO revinfo(rev, revtstmp) VALUES (" + rev + ",'" + timeStamp + "');\n");
+//    modelInsertQuery =
+//        modelInsertQuery.concat(
+//            "INSERT INTO model_aud(id, rev, revtype) VALUES ((select id from model where name='"
+//                + model.getName()
+//                + "'),"
+//                + rev
+//                + ",0);\n");
+    return modelInsertQuery;
   }
 
   public static String buildInsertIntoModelToGroupSQL(String name, List<String> modelGroups) {
